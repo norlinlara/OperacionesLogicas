de238085b7cc7992643e8b927c3f1404de9275e4
norlinlara <nslara@uth.hn>
2025-09-27 13:16:58 -0600

    Switch

--- src/Clases/CondicionalSwitch.java
+++ src/Clases/CondicionalSwitch.java
@@ -1,13 +1,37 @@
 package Clases;
 
+import java.util.Scanner;
+
 public class CondicionalSwitch {
     public void condicionalSwitch(){
+        Scanner entrada= new Scanner(System.in);
         System.out.println("----- MENU PRINCIPAL -----");
         System.out.println(" 1. Saludo ");
         System.out.println(" 2. Firmes ");
         System.out.println(" 3. Descanso ");
         System.out.println(" -1. Salir");
+        System.out.println("Ingrese una opcion: ");
+        int opcion = entrada.nextInt();
 
+        switch (opcion){
+            case 1:
+                System.out.println("HOLA!");
+                break;
+            case 2:
+                System.out.println("YA!");
+                break;
+            case 3:
+                System.out.println("*descansa*");
+                break;
+            case -1:
+                System.out.println("Hasta la vista baby......");
+                break;
+            default:
+
+                System.out.println("Opcion ingresada invalida o no existe");
+
+                break;
+        }
 
     }
 }
